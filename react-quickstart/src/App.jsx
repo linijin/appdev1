7d9ad467d96ab25a14dbfad04539ece003202630
linijin <wrthlssjane@gmail.com>
2024-10-11 22:45:17 +0800

    I'm done completing the section on rendering lists

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,26 +1,13 @@
 import './App.css'
-import AdminPanel from './AdminPanel';
-import LoginForm from './LoginForm';
+import ShoppingList from './ShoppingList';
 
 function App() {
-  let content;
-  let isLoggedIn = false;
-
-  if (isLoggedIn) {
-    content = <AdminPanel />;
-  } else {
-    content = <LoginForm />;
-  }
-
   return (
     <>
-      <h1>Welcome to my app</h1>
-      {content}
-      {/* {isLoggedIn ? (<AdminPanel />) : (<LoginForm />)} //conditional ? operator works inside JSX */}
-      {/* {isLoggedIn && <AdminPanel />} //shorter logical && syntax: */}
-
+      <ShoppingList />
     </>
   )
-}
+  }
+
 
 export default App
---
+++ react-quickstart/src/ShoppingList.jsx
@@ -0,0 +1,30 @@
+import './App.css'
+
+function ShoppingList() {
+  const products = [
+    {title: 'Cabbage', isFruit: false, id:1},
+    {title: 'Garlic', isFruit: false, id:2},
+    {title: 'Apple', isFruit: true, id:3},
+  ]
+
+  const listItems = products.map(product =>
+    <li
+      key={product.id}
+      style={{
+        color: product.isFruit ? 'magenta' : 'darkgreen'
+      }}
+      >
+        {product.title}
+    </li>
+  )
+
+  return (
+    <>
+      <h1>Welcome to my app</h1>
+      <ul>{listItems}</ul>
+    </>
+  )
+  }
+
+
+export default ShoppingList
